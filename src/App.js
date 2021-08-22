51444965553a4489b3d3765aa698376efa960e82
Sharath V <sharathkrml@gmail.com>
2021-08-22 17:04:29 +0530

    todo completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+  // const saveTodo=(e)=>{
+  //   setTodo(e.target.value)
+  //   console.log(todo);
+  // }
   return (
     <div className="app">
       <div className="mainHeading">
@@ -11,22 +17,65 @@ function App() {
         <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        {/* <input onChange={saveTodo} value={todo} type="text" placeholder="🖊️ Add item..." /> */}
+        <input
+          onChange={(e) => {
+            setTodo(e.target.value);
+          }}
+          value={todo}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i
+          onClick={() => {
+            setTodos([...todos, { id: Date.now(), text: todo, status: false }]);
+          }}
+          className="fas fa-plus"
+        ></i>
       </div>
       <div className="todos">
-        <div className="todo">
-          <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>Rect tutorial</p>
-          </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
-          </div>
-        </div>
+        {todos.map((onetodo) => {
+          return (
+            <div key={onetodo.id} className="todo">
+              <div className="left">
+                <input
+                  onChange={(e) => {
+                    setTodos(
+                      todos.filter((todo_obj) => {
+                        if (todo_obj === onetodo) {
+                          todo_obj.status = e.target.checked;
+                        }
+                        return todo_obj;
+                      })
+                    );
+                  }}
+                  checked={onetodo.status}
+                  type="checkbox"
+                  name=""
+                  id=""
+                />
+                <p>{onetodo.text}</p>
+              </div>
+              <div className="right">
+                <i
+                  onClick={() => {
+                    setTodos(todos.filter((todo) => todo !== onetodo));
+                  }}
+                  className="fas fa-times"
+                ></i>
+              </div>
+            </div>
+          );
+        })}
       </div>
+      {todos.map((i) => {
+        if (i.status) {
+          return <h1>{i.text}</h1>;
+        }
+        return null;
+      })}
     </div>
-   );
+  );
 }
 
 export default App;
